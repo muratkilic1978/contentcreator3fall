8d51f27cc882ed7d5387d9214fda394fa32a940c
Murat <muratkilic1978@gmail.com>
2017-09-18 12:19:22 +0200

    I've source-code with videogameproject
    I have updated with source-code for the videogameproject. We are going to complete the project together.

--- Day1/display-random-images-loop/index.php
+++ Day1/display-random-images-loop/index.php
@@ -1,24 +1,20 @@
- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
-<head>
-	<title>Show images from array</title>
-</head>
+	<head>
+		<title>Random images</title>
+		<link rel="stylesheet" href="css/style.css">
+	</head>
 <body>
 <?php
 
 	$pic = array('1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg','12.jpg');
 	shuffle($pic);
 ?>
-<html lang="en">
-	<head>
-		<title>Random images</title>
-		<link rel="stylesheet" href="css/style.css">
-	</head>
-	<body>
+
 		
 		<ul>
 			<?php
-				for ($i = 0; $i < 5; $i++)
+				for ($i = 0; $i < 3; $i++)
 						echo "<li><img id=\"shuffle-image\" src=\"images/$pic[$i]\"></li>";
 					
 
